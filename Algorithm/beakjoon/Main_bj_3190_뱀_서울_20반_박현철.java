ea15a1797be6e2db8c922ebbc1716422a686641a
hyuntall <71054445+hyuntall@users.noreply.github.com>
2023-02-22 16:25:25 +0900

    뱀 수정

--- Algorithm/beakjoon/Main_bj_3190_뱀_서울_20반_박현철.java
+++ Algorithm/beakjoon/Main_bj_3190_뱀_서울_20반_박현철.java
@@ -2,17 +2,11 @@ import java.io.*;
 import java.util.*;
 
 public class Main_bj_3190_뱀_서울_20반_박현철 {
-	static int N, K, L;
-	static int [][] arr;
-	static int [] dy = {0, -1, 0, 1};
-	static int [] dx = {1, 0, -1, 0};
-	static ArrayDeque<int []> snake = new ArrayDeque<>();
-	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		arr = new int[N+1][N+1];
-		K = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		int [][] arr = new int[N+1][N+1];
+		int K = Integer.parseInt(br.readLine());
 		
 		for (int i = 0; i < K; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -30,10 +24,14 @@ public class Main_bj_3190_뱀_서울_20반_박현철 {
 			comm.add(new int[] {X, C});
 		}
 		
+		int [] dy = {0, -1, 0, 1};
+		int [] dx = {1, 0, -1, 0};
+		
+		ArrayDeque<int []> snake = new ArrayDeque<>();
 		arr[1][1] = 1;
 		snake.add(new int[] {1, 1});
 		int d = 0;
-		int ans = 1;
+		int sec = 1;
 		while (true) {
 			int head [] = snake.peek();
 			int ny = head[0] + dy[d];
@@ -48,13 +46,13 @@ public class Main_bj_3190_뱀_서울_20반_박현철 {
 				arr[ny][nx] = 1;
 			} else break;
 			
-			if (!comm.isEmpty() && ans == comm.peek()[0]) {
-				if (comm.peek()[1] == 'L') d = (d + 1)%4;
+			if (!comm.isEmpty() && sec == comm.peek()[0]) {
+				int key = comm.poll()[1];
+				if (key == 'L') d = (d + 1)%4;
 				else d = (d+3)%4;
-				comm.poll();
 			}
-			ans++;
+			sec++;
 		}
-		System.out.println(ans);
+		System.out.println(sec);
 	}
 }
