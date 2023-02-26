bb11f8a66aeb1b7ef5a991d0e7084df503e4de88
hyuntall <71054445+hyuntall@users.noreply.github.com>
2023-02-26 18:30:54 +0900

    [Gold IV] Title: 게리맨더링, Time: 104 ms, Memory: 13460 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -2,66 +2,101 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N, ans = Integer.MAX_VALUE, sum;
-	static List<Integer>[] g;
-	static int [] arr;
-	static boolean []v, visited;
-	static ArrayDeque<Integer> q = new ArrayDeque<>();
-	static boolean isAble(int city, int cnt, boolean isA) {
-		q.offer(city);
-		visited[city] = true;
-		while (!q.isEmpty()) {
-			int n = q.poll();
-			cnt--;
-			for (int nCity: g[n]) {
-				if (v[nCity] == isA && !visited[nCity]) {
-					visited[nCity] = true;
-					q.offer(nCity);
-				}
-			}
-		}
-		return (cnt == 0);
-	}
-	
-	static void subs(int cnt, int aCnt) {
-		if (ans == 0) return;
-		if (cnt == N+1) {
-			if (aCnt == N || aCnt == 0) return;
-			
-			for (int i = 1; i <= N; i++) {
-				visited = new boolean[N+1];
-				if(v[i] && !isAble(i, aCnt, v[i])) return;
-				if(!v[i]&&!isAble(i, N-aCnt, v[i])) return;
-			}
-			int aSum = 0;
-			for (int i = 1; i <= N; i++) if(v[i])aSum+=arr[i];
-			ans = Math.min(ans, Math.abs(sum-aSum-aSum));
-			return;
-		}
-		v[cnt] = true;
-		subs(cnt + 1, aCnt+1);
-		v[cnt] = false;
-		subs(cnt + 1, aCnt);
-	}
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		g = new List[N+1];
-		arr = new int[N+1];
-		v = new boolean[N+1];
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 1; i <= N; i++) g[i] = new ArrayList<Integer>();
-		for (int i = 1; i <= N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			sum+=arr[i];
-		}
-		for (int i = 1; i <= N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int n = Integer.parseInt(st.nextToken());
-			for (int j = 0; j < n; j++) g[i].add(Integer.parseInt(st.nextToken()));
-		}
-		subs(1, 0);
-		System.out.println((ans!=Integer.MAX_VALUE)?ans:-1);
-	}
+    static int N;
+    static List<Integer>[] list;
+    static boolean[] v;
+    static int[] arr;
+    static int minDif = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        list = new ArrayList[N];
+        v = new boolean[N];
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            list[i] = new ArrayList<>();
+            int population = Integer.parseInt(st.nextToken());
+            arr[i] = population;
+        }
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int n = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < n; j++) {
+                // 각 리스트에 연결된 숫자는 구역 번호
+                int to = Integer.parseInt(st.nextToken()) - 1;
+                list[i].add(to);
+                //list[to].add(i);
+            }
+        }
+
+        subs(0);
+        if (minDif != Integer.MAX_VALUE)
+            System.out.println(minDif);
+        else
+            System.out.println(-1);
+        br.close();
+    }
+
+    private static void subs(int cnt) {
+        if (minDif == 0)
+            return;
+        // 인덱스가 1부터 시작이므로 N초과시 두 구역 검사
+        if (cnt == N) {
+            // a구역의 번호와 b구역의 번호를 담을 리스트
+            ArrayList<Integer> a = new ArrayList<>();
+            ArrayList<Integer> b = new ArrayList<>();
+            int aSum = 0, bSum = 0;
+            for (int i = 0; i < N; i++) {
+                if (v[i]) {
+                    a.add(i);
+                    aSum += arr[i];
+                } else {
+                    b.add(i);
+                    bSum += arr[i];
+                }
+            }
+            // 하나 이상일 경우에 둘다 연결되어 있는지 체크
+            if (aSum != 0 && bSum != 0 && isConnect(a, true) && isConnect(b, false)) {
+                minDif = Math.min(minDif, Math.abs(aSum - bSum));
+            }
+            //System.out.println();
+            return;
+        }
+        v[cnt] = true;
+        subs(cnt + 1);
+        v[cnt] = false;
+        subs(cnt + 1);
+    }
+
+    private static boolean isConnect(ArrayList<Integer> l, boolean b) {
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        boolean[] visited = new boolean[N];
+        // 시작 정점
+        //System.out.println(l);
+        int start = l.get(0);
+        visited[start] = true;
+        q.offer(start);
+        while (!q.isEmpty()) {
+            Integer i = q.poll();
+            l.remove(i);
+            //System.out.println(i);
+            // 리스트에 있는 모든 구역을 방문했으면 연결된거임
+            if (l.isEmpty()) {
+                return true;
+            }
+            // 인접구역을 큐에 담는다.
+            for (int n : list[i]) {
+                if (!visited[n]&&v[n] == b) {
+                    visited[n] = true;
+                    q.offer(n);
+                }
+            }
+        }
+        //System.out.println("false");
+        // 다돌았는데 리스트가 안비었으면 연결되지 않은 것
+        return false;
+    }
+
 }
