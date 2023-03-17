dc777b9b88ab3a38eb2a28e220436203ea79811d
hyuntall <71054445+hyuntall@users.noreply.github.com>
2023-03-17 23:47:10 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 108 ms, Memory: 12824 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(scanner.next().length());
+	}
+}
