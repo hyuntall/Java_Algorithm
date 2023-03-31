988da9ce75a3ab8bb37312933faff54762b7926a
hyuntall <71054445+hyuntall@users.noreply.github.com>
2023-03-31 11:35:51 +0900

    [Gold IV] Title: 치즈, Time: 100 ms, Memory: 12216 KB -BaekjoonHub

---
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, M;
+	static int [][] arr;
+	static int [] dy = {-1, 1, 0, 0};
+	static int [] dx = {0, 0, -1, 1};
+	static boolean [][] v;
+	static ArrayDeque<int []> q = new ArrayDeque<>();
+	static void bfs(int y, int x) {
+		q.offer(new int[] {y, x});
+		v[y][x] = true;
+		while(!q.isEmpty()) {
+			int cur[] = q.poll();
+			y = cur[0];
+			x = cur[1];
+			arr[y][x] = -1;
+			for (int i = 0; i < 4; i++) {
+				int ny = y + dy[i];
+				int nx = x + dx[i];
+				if (0<=ny&&ny<N&&0<=nx&&nx<M&&!v[ny][nx]&&arr[ny][nx]==0) {
+					v[ny][nx] = true;
+					q.offer(new int[] {ny, nx});
+				}
+			}
+		}
+	}
+	
+	static boolean melt(int y, int x) {
+		int cnt = 0;
+		for (int i = 0; i < 4; i++) {
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			if (0<=ny&&ny<N&&0<=nx&&nx<M&&arr[ny][nx]==-1)cnt++;
+			else if (0>ny||N<=ny||0>nx||M<=nx) cnt++;
+			
+		}
+		return (cnt>0)?true:false;
+	}
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N][M];
+		v = new boolean[N][M];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int j = 0; j < M; j++) arr[i][j] = Integer.parseInt(st.nextToken());
+		}
+		int chz = 0;
+		int ans = 0;
+		int last = 0;
+		bfs(0, 0);
+		ArrayDeque<int []> q = new ArrayDeque<>();
+		while (true) {
+			last = chz;
+			chz = 0;
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < M; j++) {
+					if (arr[i][j]>0) {
+						if (melt(i, j))
+							q.offer(new int [] {i, j});
+						chz++;
+					}
+				}
+			}
+			while (!q.isEmpty()) {
+				int chs[] = q.poll();
+				bfs(chs[0], chs[1]);
+			}
+			if (chz==0) break;
+			ans++;
+		}
+		System.out.println(ans);
+		System.out.println(last);
+	}
+}
