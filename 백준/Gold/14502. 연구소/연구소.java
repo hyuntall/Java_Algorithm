124450dd3ec2fc22e5fc959ff289d43ac76b0ad2
hyuntall <71054445+hyuntall@users.noreply.github.com>
2023-10-08 22:55:19 +0900

    [Gold IV] Title: 연구소, Time: 896 ms, Memory: 298984 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -2,65 +2,74 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N, M, ans = 0;
-	static int [][] arr, newArr;
-	static int [] comb = new int [3];
+	static int N, M;
+	static int [][] arr;
 	static int [] dy = {-1, 1, 0, 0};
 	static int [] dx = {0, 0, -1, 1};
-	static List <int[]> virus = new ArrayList<>();
+	static int [][] wall = new int[3][2];
+	static int ans;
+	static List<int []> initVirus = new ArrayList<int[]>();
+	static ArrayDeque<int []> virus = new ArrayDeque<>();
 	
-	static int count() {
+	static int getArea(int [][] arr) {
 		int sum = 0;
-		for (int i = 0; i < N; i++) 
-			for (int j = 0; j < M; j++)
-				if (newArr[i][j]==0)sum++;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (arr[i][j]==0)sum++;
+			}
+		}
 		return sum;
 	}
 	
-	static void dfs() {
-		ArrayDeque<int []> q = new ArrayDeque<>();
-		for (int [] v : virus) q.add(v);
-		while (!q.isEmpty()) {
-			int [] v = q.poll();
-			int y = v[0], x = v[1];
-			newArr[y][x] = 2;
+	static void bfs(int [][] arr) {
+		while (!virus.isEmpty()) {
+			int [] cur = virus.poll();
+			arr[cur[0]][cur[1]] = 2;
 			for (int i = 0; i < 4; i++) {
-				int ny = y + dy[i];
-				int nx = x + dx[i];
-				if (0<=ny&&ny<N&&0<=nx&&nx<M&&newArr[ny][nx]==0)
-					q.add(new int[] {ny,nx});
+				int ny = cur[0] + dy[i];
+				int nx = cur[1] + dx[i];
+				
+				if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
+				if (arr[ny][nx] != 0) continue;
+				virus.offer(new int[] {ny, nx});
 			}
 		}
+		
 	}
 	
-	static void comb(int cnt, int idx) {
+	static void comb(int cnt, int index) {
 		if (cnt == 3) {
-			for (int i=0; i<N; i++) newArr[i]=Arrays.copyOf(arr[i], M);
-			for(int i : comb) newArr[i/M][i%M] = 1;
-			dfs();
-			int area = count();
-			ans = area>ans?area:ans;
+			int [][] cpArr = new int[N][M];
+			for (int i = 0; i < N; i++) cpArr[i] = Arrays.copyOf(arr[i], M);
+			for (int i = 0; i < 3; i++) 
+				cpArr[wall[i][0]][wall[i][1]] = 1;
+			
+			for (int [] v:initVirus) virus.offer(v);
+			bfs(cpArr);
+			ans = Math.max(ans, getArea(cpArr));
 			return;
 		}
-		for (int i=idx; i<N*M; i++) {
-			if (arr[i/M][i%M]!=0) continue;
-			comb[cnt] = i;
+		
+		for (int i = index; i < N*M; i++) {
+			if (arr[i/M][i%M] != 0) continue;
+			wall[cnt][0] = i/M;
+			wall[cnt][1] = i%M;
 			comb(cnt+1, i+1);
 		}
 	}
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		arr = new int [N][M];
-		newArr = new int[N][M];
 		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
+			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
-				if (arr[i][j] == 2) virus.add(new int[] {i, j});
+				if (arr[i][j] == 2) 
+					initVirus.add(new int[] {i, j});
 			}
 		}
 		comb(0, 0);
